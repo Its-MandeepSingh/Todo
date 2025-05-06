159173c6ff7adf8b65d892f9993d0057130435dd
Its-MandeepSingh <mandeepsinghkumawat15@gmail.com>
2025-05-06 08:53:40 +0530

    new length

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -6,9 +6,8 @@ const Todo = ({ val, onDelete }) => {
   
   return (
     <>
-      <div>
-        <div className="bg-blue-500 transition delay-2 duration-100 ease-in-out hover:-translate-y-1   hover:scale-101  hover:bg-cyan-800 :hover text-white w-auto h-12 font-bold font-mono mt-1 flex flex-row justify-between items-center pr-10 pl-1.5 max-sm:h-10 ">
-          {val}
+        <div className="bg-blue-500 transition delay-2 duration-100 ease-in-out hover:-translate-y-1   hover:scale-101  hover:bg-cyan-800 :hover text-white w-full h-12 font-bold font-mono mt-1 flex flex-row justify-between items-center pr-10 pl-1.5 max-sm:h-10 ">
+          {val.length>=65? val.slice(0,65):val}
           <div className=" flex gap-10 max-sm:gap-2">
             <TiTick className="text-3xl w-10 border-2  text-amber-300 hover:text-cyan-950 max-sm:h-5 border-none" />
             
@@ -19,7 +18,6 @@ const Todo = ({ val, onDelete }) => {
           </div>
         </div>
         {/* 'event' work  for handling event capturing */}
-      </div>
     </>
   );
 };
